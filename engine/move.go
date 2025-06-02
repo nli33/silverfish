9c1d188c8210ffeda977f689d2b841eb7b46a798
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2025-06-02 18:42:04 -0400

    expand the Move struct

--- engine/move.go
+++ engine/move.go
@@ -1,11 +1,29 @@
 package engine
 
-type Move struct {
-	FromSquare     Square
-	ToSquare       Square
-	Piece          uint8
-	CapturedPiece  uint8
-	PromotionPiece uint8
-	IsCastle       bool
-	IsEnPassant    bool
+/*
+0-5: from square
+6-11: to square
+12-13: promotion piece (knight, bishop, rook, queen)
+14-15: castling, en passant, promotion
+*/
+type Move uint16
+
+func NewMove(from Square, to Square) Move {
+	return Move(from<<6 | to)
+}
+
+func (m Move) From() Square {
+	return Square(m & 0b111111)
+}
+
+func (m Move) To() Square {
+	return Square(m >> 6 & 0b111111)
+}
+
+func (m Move) Promotion() uint8 {
+	return uint8(m>>12&0b11 + 1)
+}
+
+func (m Move) Type() uint8 {
+	return uint8(m >> 14)
 }
