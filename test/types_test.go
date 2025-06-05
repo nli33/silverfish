f717d832cc49d97a8bde907957e265255c7192d5
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2025-06-04 21:05:09 -0400

    add types test (mainly Move-related for now)

---
+++ test/types_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"fmt"
+	"silverfish/engine"
+	"testing"
+)
+
+func TestMoveStr(t *testing.T) {
+	gotStr := engine.NewMoveFromStr("b2b3").ToString()
+	wantStr := "b2b3"
+	if gotStr != wantStr {
+		t.Errorf("NewMoveFromStr(b2b3).ToString()")
+		fmt.Printf("Got: %s\n", gotStr)
+		fmt.Printf("Want: %s\n", wantStr)
+	}
+
+	gotStr = engine.NewMoveFromStr("b2b1q").ToString()
+	wantStr = "b2b1q"
+	if gotStr != wantStr {
+		t.Errorf("NewMoveFromStr(b2b1q).ToString()")
+		fmt.Printf("Got: %s\n", gotStr)
+		fmt.Printf("Want: %s\n", wantStr)
+	}
+
+	gotMove := engine.NewMoveFromStr("g6h5")
+	wantMove := engine.NewMove(engine.SquareG6, engine.SquareH5)
+	if gotMove != wantMove {
+		t.Errorf("NewMoveFromStr(g6h5)")
+		fmt.Printf("Got: %016b\n", gotMove)
+		fmt.Printf("Want: %016b\n", wantMove)
+	}
+
+	gotMove = engine.NewMoveFromStr("b2b1q")
+	wantMove = engine.NewMovePromotion(engine.SquareB2, engine.SquareB1, engine.Queen)
+	if gotMove != wantMove {
+		t.Errorf("NewMoveFromStr(b2b1q)")
+		fmt.Printf("Got: %016b\n", gotMove)
+		fmt.Printf("Want: %016b\n", wantMove)
+	}
+}
