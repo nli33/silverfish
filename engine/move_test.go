7d30534a5ff6381924ab12dfc385f2fb9d885d9e
EarthTraveller1 <liangyuezhao7@gmail.com>
2025-06-09 15:46:45 -0400

    Added a test for the function to conver the bitboard into moves.

---
+++ engine/move_test.go
@@ -0,0 +1,22 @@
+package engine_test
+
+import (
+	"silverfish/engine"
+	"testing"
+)
+
+func GetMovesFromBitboardTest(t *testing.T) {
+	gotMoves := engine.GetMovesFromBitboard(engine.SquareA1, 0x000000000101010e)
+	expectedMoves := []engine.Move{
+		engine.NewMove(engine.SquareA1, engine.SquareA2),
+		engine.NewMove(engine.SquareA1, engine.SquareA3),
+		engine.NewMove(engine.SquareA1, engine.SquareA4),
+		engine.NewMove(engine.SquareA1, engine.SquareB1),
+		engine.NewMove(engine.SquareA1, engine.SquareC1),
+		engine.NewMove(engine.SquareA1, engine.SquareD1),
+	}
+
+	if !equalSets(gotMoves, expectedMoves) {
+		t.Errorf("Expected: %#v\nGot: %#v\n", expectedMoves, gotMoves)
+	}
+}
