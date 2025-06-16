e629e35f42ce2f6b04fe940659586ca4fec2ddce
EarthTraveller1 <liangyuezhao7@gmail.com>
2025-06-16 15:02:16 -0400

    Sends the Uciok message _after_ the bitboard is initialized.

--- cmd/silverfish/main.go
+++ cmd/silverfish/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"silverfish/engine"
+	"fmt"
+	"os"
 )
 
 func main() {
 	engine.Init()
 	engine.InitBitboard()
+
+	err := engine.UciOk()
+	// This should not happen, ideally, but if it does, something is deeply wrong
+	// with the program.
+	if err != nil {
+		_ = fmt.Errorf("WHAT THE FUCK??")
+		os.Exit(69)
+	}
 }
--- engine/init.go
+++ engine/init.go
@@ -1,9 +1,7 @@
 package engine
 
 import (
-	"fmt"
 	"math/rand"
-	"os"
 )
 
 var Rng *rand.Rand = rand.New(rand.NewSource(123))
@@ -13,11 +11,4 @@ func Init() {
 	UciSetAuthor("silverfish developers")
 	UciSetProtocol(2)
 
-	err := UciOk()
-	// This should not happen, ideally, but if it does, something is deeply wrong
-	// with the program.
-	if err != nil {
-		fmt.Errorf("WHAT THE FUCK??")
-		os.Exit(69)
-	}
 }
