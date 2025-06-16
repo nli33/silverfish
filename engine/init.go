3ff421c32e6f5e485566cd6b2f18f0806a9ef3a1
EarthTraveller1 <liangyuezhao7@gmail.com>
2025-06-16 14:40:41 -0400

    Made the init thing use UCI instead of doing nothing.

--- engine/init.go
+++ engine/init.go
@@ -3,10 +3,21 @@ package engine
 import (
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 var Rng *rand.Rand = rand.New(rand.NewSource(123))
 
 func Init() {
-	fmt.Println("HELLO")
+	UciSetEngineName("silverfish")
+	UciSetAuthor("silverfish developers")
+	UciSetProtocol(2)
+
+	err := UciOk()
+	// This should not happen, ideally, but if it does, something is deeply wrong
+	// with the program.
+	if err != nil {
+		fmt.Errorf("WHAT THE FUCK??")
+		os.Exit(69)
+	}
 }
