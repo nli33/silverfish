7280aeeda8ee5bdad11f1ebaa3acdbafbc523ef0
EarthTraveller1 <liangyuezhao7@gmail.com>
2025-06-16 16:04:52 -0400

    Waited for UCI + did the hello.

--- engine/init.go
+++ engine/init.go
@@ -1,14 +1,24 @@
 package engine
 
 import (
+	"fmt"
 	"math/rand"
 )
 
 var Rng *rand.Rand = rand.New(rand.NewSource(123))
 
 func Init() {
+	// Wait for the uci command
+	for {
+		command := ""
+		fmt.Scan(&command)
+		if command == "uci" {
+			break
+		}
+	}
+
 	UciSetEngineName("silverfish")
 	UciSetAuthor("silverfish developers")
 	UciSetProtocol(2)
-
+	UciError("HELLO")
 }
