35ffb5dc3a6b81e4b09dd51f10c9d8363fcc622a
EarthTraveller1 <liangyuezhao7@gmail.com>
2025-06-16 15:17:36 -0400

    Added a function to handle the messages from the client.

--- engine/uci.go
+++ engine/uci.go
@@ -1,6 +1,10 @@
 package engine
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
 
 const (
 	UciInitialState = iota
@@ -25,6 +29,51 @@ func NewUciError(err string) *UciErrorType {
 
 func (err *UciErrorType) Error() string {
 	return err.err
+}
+
+func UciHandleMessages(stdin bufio.Scanner, pos *Position) {
+	result := stdin.Scan()
+	if !result {
+		UciError("I/O error or some shit.")
+		return
+	}
+
+	message := stdin.Text()
+
+	switch uci_state {
+	case UciIdleState:
+		if strings.HasPrefix(message, "position") {
+			position := strings.SplitN(message, " ", 2)
+			if position[1] == "fen" && len(position) > 2 {
+				*pos = FromFEN(position[2])
+			} else if position[1] == "startpos" {
+				*pos = NewPosition()
+			}
+		} else if message == "go" {
+			uci_state = UciActiveState
+		} else if message == "isready" {
+			uci_state = UciSyncState
+		} else {
+			UciInfo("Not implemented")
+		}
+
+		break
+	case UciActiveState:
+		switch message {
+		case "isready":
+			uci_state = UciPingState
+			break
+		case "stop":
+			uci_state = UciHaltState
+			break
+		default:
+			UciError(fmt.Sprintf("Sir, it is currently the %d state. You can't do %s", uci_state, message))
+		}
+
+		break
+	default:
+		UciError(fmt.Sprintf("Sir, it is currently the %d state. You can't do %s", uci_state, message))
+	}
 }
 
 func UciOk() error {
