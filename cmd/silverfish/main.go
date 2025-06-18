1ad7c19cd954d0c470472cc4a006dbad5446a65f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-06-17 20:29:10 -0400

    Implemented the go perft depth <depth> command.

--- cmd/silverfish/main.go
+++ cmd/silverfish/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"silverfish/engine"
 )
@@ -14,13 +15,24 @@ func HandleMessages(channel chan engine.UciClientMessage) {
 		channel <- message
 
 		quit_message := message.MessageType == engine.UciQuitClientMessage
-		engine_idle := engine.UciState == engine.UciIdleState
 		// Basically, stop listening for more messages after a valid quit message
 		// is received.
-		if quit_message && engine_idle {
+		if quit_message {
 			return
 		}
 	}
+}
+
+func executeGoCommand(channel chan bool, position *engine.Position, command *engine.UciGoMessage) {
+	// Currently, only Perft is implemented, as that was all that was demanded.
+	if command.Perft && command.Depth != 0 {
+		engine.UciInfo("Perft started.")
+		result := engine.Perft(position, int(command.Depth))
+		engine.UciInfo(fmt.Sprintf("Perft result: %d", result))
+	}
+
+	// Tell the main thread that we're done.
+	channel <- true
 }
 
 func main() {
@@ -28,10 +40,16 @@ func main() {
 	engine.InitBitboard()
 
 	messageChannel := make(chan engine.UciClientMessage, 5)
+	// Used for reporting if an action is done.
+	actionAlertChannel := make(chan bool)
+	active := false
+
+	position := engine.StartingPosition()
 
 	go HandleMessages(messageChannel)
 
-	mainloop: for {
+mainloop:
+	for {
 		message := engine.UciClientMessage{}
 		select {
 		case message = <-messageChannel:
@@ -40,17 +58,30 @@ func main() {
 			continue
 		}
 
-		switch message.MessageType {
-		case engine.UciUciClientMessage:
-			engine.UciSetEngineName("Silverfish 0.0.69a")
-			engine.UciSetAuthor("李能和赵梁越")
-			engine.UciSetProtocol(2)
+		if active {
+			select {
+			case _ = <-actionAlertChannel:
+				active = false
+			default:
+				// Do nothing :ye:
+			}
+		} else {
+			switch message.MessageType {
+			case engine.UciUciClientMessage:
+				engine.UciSetEngineName("Silverfish 0.0.69a")
+				engine.UciSetAuthor("李能和赵梁越")
+				engine.UciSetProtocol(2)
 
-			engine.UciOk()
-		case engine.UciIsReadyClientMessage:
-			engine.UciReadyOk()
-		case engine.UciQuitClientMessage:
-			break mainloop
+				engine.UciOk()
+			case engine.UciIsReadyClientMessage:
+				engine.UciReadyOk()
+			case engine.UciPositionClientMessage:
+				position = *message.Position
+			case engine.UciQuitClientMessage:
+				break mainloop
+			case engine.UciGoClientMessage:
+				go executeGoCommand(actionAlertChannel, &position, message.GoMessage)
+			}
 		}
 	}
 }
