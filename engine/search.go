1adcc1588083a471bfc900bef55719666dc18264
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2025-06-19 10:21:24 -0400

    feat: basic alpha-beta implementation

---
+++ engine/search.go
@@ -0,0 +1,54 @@
+package engine
+
+// based on negamax (flip sign), each player maximizes their own score
+// alpha: best score guaranteed for max-player. can prune branches that give less than this
+// beta: upper limit that min-player will tolerate. min-player will prune lines exceeding this
+
+func AlphaBeta(pos Position, depth int) (int32, Move) {
+	alpha := -Infinity // best score of max-player that is guaranteed
+	beta := Infinity   // worst score that minimizing player tolerates
+	bestScore := -Infinity
+	var bestMove Move
+
+	for _, move := range pos.LegalMoves() {
+		pos.DoMove(move)
+		score := -alphaBetaInner(pos, -beta, -alpha, depth-1)
+		pos.UndoMove(move)
+
+		if score > bestScore {
+			bestScore = score
+			bestMove = move
+		}
+		if score > alpha {
+			alpha = score
+		}
+	}
+
+	return bestScore, bestMove
+}
+
+func alphaBetaInner(pos Position, alpha int32, beta int32, depth int) int32 {
+	if depth == 0 {
+		return Evaluate(&pos)
+	}
+
+	bestScore := -Infinity
+
+	for _, move := range pos.LegalMoves() {
+		pos.DoMove(move)
+		score := -alphaBetaInner(pos, -beta, -alpha, depth-1)
+		pos.UndoMove(move)
+
+		if score >= beta {
+			return score
+		}
+		if score > bestScore {
+			bestScore = score
+		}
+		if score > alpha {
+			alpha = score
+		}
+	}
+
+	return bestScore
+}
