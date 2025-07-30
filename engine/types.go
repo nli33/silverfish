6d9f970929e42e11b4bdf3538d31ee685357aea4
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2025-07-30 12:12:00 -0400

    add basic piece square table

--- engine/types.go
+++ engine/types.go
@@ -155,6 +155,17 @@ const (
 var BishopDirections = []int8{NorthEast, SouthEast, NorthWest, SouthWest}
 var RookDirections = []int8{North, South, East, West}
 
+var FlipSq = [64]Square{
+	SquareA8, SquareB8, SquareC8, SquareD8, SquareE8, SquareF8, SquareG8, SquareH8,
+	SquareA7, SquareB7, SquareC7, SquareD7, SquareE7, SquareF7, SquareG7, SquareH7,
+	SquareA6, SquareB6, SquareC6, SquareD6, SquareE6, SquareF6, SquareG6, SquareH6,
+	SquareA5, SquareB5, SquareC5, SquareD5, SquareE5, SquareF5, SquareG5, SquareH5,
+	SquareA4, SquareB4, SquareC4, SquareD4, SquareE4, SquareF4, SquareG4, SquareH4,
+	SquareA3, SquareB3, SquareC3, SquareD3, SquareE3, SquareF3, SquareG3, SquareH3,
+	SquareA2, SquareB2, SquareC2, SquareD2, SquareE2, SquareF2, SquareG2, SquareH2,
+	SquareA1, SquareB1, SquareC1, SquareD1, SquareE1, SquareF1, SquareG1, SquareH1,
+}
+
 var Default = [2][6]Bitboard{
 	{
 		Bitboard(0xff00),
